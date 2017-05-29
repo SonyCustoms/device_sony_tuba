666a100afee78aa8326fcbb546957332357b9f3c
Alberto Guanti <albertoguanti@gmail.com>
2017-05-29 22:14:41 +0200

    k11ta_a: switch COMMON_GLOBAL_CFLAGS for Android N

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,8 +25,8 @@ MTK_INTERNAL_CDEFS := $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_NAME),$(if $(filte
 MTK_INTERNAL_CDEFS += $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_VALUE),$(if $(filter-out no NO none NONE false FALSE,$($(t))),$(foreach v,$(shell echo $($(t)) | tr '[a-z]' '[A-Z]'),-D$(v))))
 MTK_INTERNAL_CDEFS += $(foreach t,$(AUTO_ADD_GLOBAL_DEFINE_BY_NAME_VALUE),$(if $(filter-out no NO none NONE false FALSE,$($(t))),-D$(t)=\"$($(t))\"))
 
-COMMON_GLOBAL_CFLAGS += $(MTK_INTERNAL_CDEFS)
-COMMON_GLOBAL_CPPFLAGS += $(MTK_INTERNAL_CDEFS)
+BOARD_GLOBAL_CFLAGS += $(MTK_INTERNAL_CDEFS)
+BOARD_GLOBAL_CPPFLAGS += $(MTK_INTERNAL_CDEFS)
 
 # Kernel informations
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
