eb3037acbc12306b490834b2c0f6384444c15abc
Andrew <poisk567@gmail.com>
2018-09-30 14:39:49 +0300

    try to fix wifi

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensor Calibration
 PRODUCT_PACKAGES += \
-libem_sensor_jni
+    libem_sensor_jni
 
 PRODUCT_PACKAGES += \
     Snap \
@@ -61,10 +61,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf \
     $(LOCAL_PATH)/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf \
     $(LOCAL_PATH)/configs/ht120.mtc:system/etc/.tp/.htc120.mtc
-
-# System Properties
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -137,22 +133,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
-
-# Packages
-PRODUCT_PACKAGES += \
-    libwpa_client \
-    lib_driver_cmd_mt66xx \
-    hostapd \
-    wpa_supplicant
-
-# Configurations
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
-    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # Configurations
 PRODUCT_COPY_FILES += \
