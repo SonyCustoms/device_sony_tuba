262780da97c2a38e765e995ec86ea9c69b294292
Andrew <poisk567@gmail.com>
2019-08-10 21:11:18 +0300

    get shim in one lib 2/5
    remove gui.cpp from this directory

--- libtuba/tuba_gui.cpp
+++ libtuba/tuba_gui.cpp
@@ -25,4 +25,12 @@ extern "C" {
         _ZN7android18BufferItemConsumerC2ERKNS_2spINS_22IGraphicBufferConsumerEEEyib(consumer, static_cast<uint64_t>(consumerUsage), bufferCount, controlledByApp);
     }
 #endif
+    
+    void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+    void *outProducer, void *outConsumer, bool consumerIsSurfaceFlinger);
+
+    void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
+    void *outProducer, void *outConsumer, void *allocator __unused) {
+  _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+      outProducer, outConsumer, false);
 }
