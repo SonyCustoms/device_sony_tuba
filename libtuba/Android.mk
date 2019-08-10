826f7f2acd821496af51d8ab661af634793278d1
Andrew <poisk567@gmail.com>
2019-08-10 21:09:25 +0300

    get shim in one lib 1/5

--- libtuba/Android.mk
+++ libtuba/Android.mk
@@ -4,23 +4,12 @@ ifeq ($(TARGET_INCLUDE_LIBTUBA),true)
     include $(CLEAR_VARS)
     LOCAL_MODULE := libtuba
     LOCAL_MODULE_TAGS := optional
-    LOCAL_SHARED_LIBRARIES := libbinder libc liblog libicuuc libicui18n libmedia
+    LOCAL_SHARED_LIBRARIES := libbinder libc liblog libicuuc libicui18n libmedia libgui libui libutils
     LOCAL_SRC_FILES := \
         tuba_audio.cpp \
         tuba_omx.cpp \
         tuba_xlog.c \
-        tuba_sensor.cpp
-
-include $(BUILD_SHARED_LIBRARY)
-endif
-
-ifeq ($(TARGET_INCLUDE_LIBGUI),true)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libtuba_gui
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libgui libui libutils
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES := \
-    gui.cpp
+        tuba_sensor.cpp \
+        tuba_gui.cpp
 include $(BUILD_SHARED_LIBRARY)
 endif
