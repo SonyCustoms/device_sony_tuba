6a8f9268d62d94abf4a0064d40156d6eef6cca10
Andrew <poisk567@gmail.com>
2019-08-15 06:47:05 +0300

    remove releasetools

--- releasetools.py
+++
@@ -1,9 +0,0 @@
-def FullOTA_Assertions(info):
-    info.script.AppendExtra('ui_print("=============================================");');
-    info.script.AppendExtra('ui_print("=============================================");');
-    info.script.AppendExtra('ui_print("                 LineageOS 14.1              ");');
-    info.script.AppendExtra('ui_print("                  For Sony XA                ");');
-    info.script.AppendExtra('ui_print("                     Fxxx                    ");');
-    info.script.AppendExtra('ui_print("                AndroidOS v7.1.2             ");');
-    info.script.AppendExtra('ui_print("=============================================");');
-    info.script.AppendExtra('ui_print("============With love from Ukraine!==========");');
